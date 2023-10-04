13705485da13646e5da36505b390209ceb1d3878
aecong <dkdus78655@gmail.com>
2023-10-04 18:44:47 +0900

    Boy 클래스 생성

--- Lecture09_Game_Objects/boy_grass_object.py
+++ Lecture09_Game_Objects/boy_grass_object.py
@@ -11,6 +11,18 @@ class Grass:
 
     def update(self): pass
 
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
 
 def handle_events():
     global running
@@ -25,6 +37,7 @@ def handle_events():
 def reset_world():
     global running
     global grass
+    
 
     running = True
     grass = Grass()
